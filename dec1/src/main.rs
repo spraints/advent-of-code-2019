2d8023fa5ae87f848c36085939d4e0964e8ca758
Matt Burke <spraints@gmail.com>
2019-12-21 16:59:49 -0500

    day 1: count lines

--- dec1/src/main.rs
+++ dec1/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting Fuel Counter-Upper!");
+
+    let mut fuel = 0;
+
+    loop {
+        let mut line = String::new();
+        let bytes = io::stdin().read_line(&mut line).expect("Error reading from stdin");
+        if bytes == 0 {
+            break;
+        }
+        match line.trim().parse() {
+            Err(error) => println!("error parsing {}: {}", line.trim(), error),
+            Ok(weight) => fuel += calc_fuel(weight),
+        }
+    }
+
+    println!("got it: {}", fuel);
+}
+
+fn calc_fuel(weight: u32) -> u32 {
+    return weight / weight;
 }
