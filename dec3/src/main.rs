5c9287bd491a9d27a8079d019f4d2dde24ff30f5
Matt Burke <spraints@gmail.com>
2019-12-24 23:04:45 -0500

    keep track of wire length, too

--- dec3/src/main.rs
+++ dec3/src/main.rs
@@ -4,7 +4,8 @@ use std::io;
 type Segment = (char, u32);
 type Path = Vec<Segment>;
 type Coord = (i32, i32);
-type Location = (u8, u32);
+type WalkState = (Coord, u32);
+type Location = (u8, u32, u32); // names, manhattan distance, cumulative wire length.
 type Grid = HashMap<Coord, Location>;
 
 fn main() {
@@ -15,48 +16,64 @@ fn main() {
     plot(&mut grid, read_path(), 0x01);
     plot(&mut grid, read_path(), 0x02);
 
-    let res = grid.values().fold(10000000, |res, (wires, dist)| {
+    let res = grid.values().fold(10000000, |res, (wires, dist, _)| {
         if *wires == 0x03 && dist < &res {
             *dist
         } else {
             res
         }
     });
-    println!("MIN DISTANCE: {}", res);
+    println!("MIN TAXI DISTANCE: {}", res);
+
+    let res = grid.values().fold(10000000, |res, (wires, _, dist)| {
+        if *wires == 0x03 && dist < &res {
+            *dist
+        } else {
+            res
+        }
+    });
+    println!("MIN WIRE DISTANCE: {}", res);
 }
 
 fn plot(mut grid: &mut Grid, path: Path, wire_name: u8) {
-    let mut coords = (0, 0);
+    let mut state = ((0,0), 0);
     for segment in path {
-        println!("{:?} --> {:?}", coords, segment);
-        coords = match segment {
-            ('R', n) => walk(&mut grid, coords, wire_name, n, |(x, y)| (x + 1, y)),
-            ('L', n) => walk(&mut grid, coords, wire_name, n, |(x, y)| (x - 1, y)),
-            ('U', n) => walk(&mut grid, coords, wire_name, n, |(x, y)| (x, y + 1)),
-            ('D', n) => walk(&mut grid, coords, wire_name, n, |(x, y)| (x, y - 1)),
+        println!("{:?} --> {:?}", state, segment);
+        state = match segment {
+            ('R', n) => walk(&mut grid, state, wire_name, n, |(x, y)| (x + 1, y)),
+            ('L', n) => walk(&mut grid, state, wire_name, n, |(x, y)| (x - 1, y)),
+            ('U', n) => walk(&mut grid, state, wire_name, n, |(x, y)| (x, y + 1)),
+            ('D', n) => walk(&mut grid, state, wire_name, n, |(x, y)| (x, y - 1)),
             other => {
                 println!("ERR unrecognized segment for {}: {:?}", wire_name, other);
-                coords
+                state
             }
         };
     }
-    println!("{:?}", coords);
+    println!("{:?}", state);
 }
 
-fn walk<F>(grid: &mut Grid, coords: Coord, wire_name: u8, count: u32, step: F) -> Coord
+fn walk<F>(grid: &mut Grid, state: WalkState, wire_name: u8, count: u32, step: F) -> WalkState
 where
     F: Fn(Coord) -> Coord,
 {
-    let mut res = coords;
+    let (mut coords, mut dist) = state;
     for _ in 0..count {
-        res = step(res);
+        coords = step(coords);
+        dist = dist + 1;
         // TODO fix mutable/immutable borrowing conflict here.
-        match grid.get(&res) {
-            None => grid.insert(res, (wire_name, calc_dist(&res))),
-            Some((wire_names, dist)) => grid.insert(res, (wire_names | wire_name, *dist)),
+        match grid.get(&coords) {
+            None => grid.insert(coords, (wire_name, calc_dist(&coords), dist)),
+            Some((wire_names, taxidist, wiredist)) => {
+                let mut newwiredist = *wiredist;
+                if wire_names & wire_name == 0 {
+                    newwiredist = newwiredist + dist;
+                }
+                grid.insert(coords, (wire_names | wire_name, *taxidist, newwiredist))
+            },
         };
     }
-    res
+    (coords, dist)
 }
 
 fn calc_dist(coords: &Coord) -> u32 {
