06868de027a02344e870b8bfa50baab5025750de
Matt Burke <spraints@gmail.com>
2019-12-29 22:50:53 -0500

    day 6: Find a route from YOU to SAN

--- dec6/src/main.rs
+++ dec6/src/main.rs
@@ -10,8 +10,7 @@ struct Orbit {
 type Orbits = Vec<Orbit>;
 
 struct OrbObj {
-    name: String,
-    center: String,
+    center: Option<String>,
     satellites: Vec<String>,
 }
 
@@ -23,6 +22,51 @@ fn main() {
     let orbits = make_graph(orbits);
 
     println!("TOTAL ORBITS: {}", walk_dist(&orbits, "COM"));
+
+    let path = find_path(&orbits, "YOU", "SAN").unwrap();
+    println!("DISTANCE FROM 'YOU' TO 'SAN': {}", path.len() - 1);
+    println!("   {:?}", path);
+}
+
+fn find_path(objs: &OrbitGraph, from: &str, to: &str) -> Result<Vec<String>, String> {
+    let from = from.to_string();
+    let to = to.to_string();
+    match objs.get(&from) {
+        None => Err("FROM not found".to_string()),
+        Some(from) => match objs.get(&to) {
+            None => Err("TO not found".to_string()),
+            Some(to) => intersecting_paths(
+                walk_to_root(objs, &from.center, vec![]),
+                walk_to_root(objs, &to.center, vec![]),
+            ),
+        },
+    }
+}
+
+fn walk_to_root(objs: &OrbitGraph, start: &Option<String>, mut accum: Vec<String>) -> Vec<String> {
+    match start {
+        None => accum,
+        Some(next_name) => {
+            accum.push(next_name.to_string());
+            match objs.get(next_name) {
+                None => accum,
+                Some(next_obj) => walk_to_root(objs, &next_obj.center, accum),
+            }
+        }
+    }
+}
+
+fn intersecting_paths(path1: Vec<String>, path2: Vec<String>) -> Result<Vec<String>, String> {
+    println!("p1: {:?}", path1);
+    println!("p2: {:?}", path2);
+    let overlap = path1.iter().rev().zip(path2.iter().rev()).filter(|(item1, item2)| item1 == item2).count();
+    if overlap == 0 {
+        Err("no overlap!".to_string())
+    } else {
+        let path1 = path1.iter().rev().skip(overlap - 1);
+        let path2 = path2.iter().rev().skip(overlap);
+        Ok(path1.rev().chain(path2).map(|s| s.to_string()).collect())
+    }
 }
 
 fn walk_dist(objs: &OrbitGraph, center: &str) -> u32 {
@@ -76,14 +120,13 @@ fn make_graph(orbits: Orbits) -> OrbitGraph {
                 res.insert(
                     orbit.satellite.to_string(),
                     OrbObj {
-                        name: orbit.satellite.to_string(),
-                        center: orbit.center.to_string(),
+                        center: Some(orbit.center.to_string()),
                         satellites: vec![],
                     },
                 );
             }
             Some(obj) => {
-                obj.center = orbit.center.to_string();
+                obj.center = Some(orbit.center.to_string());
             }
         };
         match res.get_mut(&orbit.center) {
@@ -91,8 +134,7 @@ fn make_graph(orbits: Orbits) -> OrbitGraph {
                 res.insert(
                     orbit.center.to_string(),
                     OrbObj {
-                        name: orbit.center.to_string(),
-                        center: "??".to_string(),
+                        center: None,
                         satellites: vec![orbit.satellite.to_string()],
                     },
                 );
