00ebaec398e69739fb4817cd370fbbe737686d6a
Matt Burke <spraints@gmail.com>
2020-01-04 18:26:43 -0500

    replace dec7/README with tests

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -3,6 +3,56 @@ use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ex1() {
+        let program = vec![
+            3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0,
+        ];
+        assert_eq!(43210, part1(&program, false));
+    }
+
+    #[test]
+    fn test_ex2() {
+        let program = vec![
+            3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23,
+            99, 0, 0,
+        ];
+        assert_eq!(54321, part1(&program, false));
+    }
+
+    #[test]
+    fn test_ex3() {
+        let program = vec![
+            3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1,
+            33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0,
+        ];
+        assert_eq!(65210, part1(&program, false));
+    }
+
+    #[test]
+    fn test_ex4() {
+        let program = vec![
+            3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1,
+            28, 1005, 28, 6, 99, 0, 0, 5,
+        ];
+        assert_eq!(139629729, part2(&program, false));
+    }
+
+    #[test]
+    fn test_ex5() {
+        let program = vec![
+            3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+            -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+            53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10,
+        ];
+        assert_eq!(18216, part2(&program, false));
+    }
+}
+
 fn main() {
     println!("--------------");
     println!("INTCODE ONLINE");
@@ -15,7 +65,7 @@ fn main() {
     part2(&program, false);
 }
 
-fn part2(program: &IntCodeMemory, strict: bool) {
+fn part2(program: &IntCodeMemory, strict: bool) -> i32 {
     println!("PART 2");
     println!("------");
     let mut max_out = 0;
@@ -26,6 +76,7 @@ fn part2(program: &IntCodeMemory, strict: bool) {
         }
     }
     println!("MAX OUTPUT: {}", max_out);
+    max_out
 }
 
 fn try_inputs2(program: &IntCodeMemory, inputs: [i32; 5], strict: bool) -> i32 {
@@ -105,7 +156,7 @@ fn try_inputs2(program: &IntCodeMemory, inputs: [i32; 5], strict: bool) -> i32 {
     }
 }
 
-fn part1(program: &IntCodeMemory, strict: bool) {
+fn part1(program: &IntCodeMemory, strict: bool) -> i32 {
     println!("PART 1");
     println!("------");
     let mut max_out = 0;
@@ -116,6 +167,7 @@ fn part1(program: &IntCodeMemory, strict: bool) {
         }
     }
     println!("MAX OUTPUT: {}", max_out);
+    max_out
 }
 
 fn try_inputs(program: &IntCodeMemory, inputs: [i32; 5], strict: bool) -> i32 {
