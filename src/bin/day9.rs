ee8b4daab562b197a5b01acbed7f897c3510db4c
Matt Burke <spraints@gmail.com>
2020-01-11 07:58:14 -0500

    run part 2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,4 +1,4 @@
-use spraints_advent_of_code_2019::intcode::{self, IntCodeMemory};
+use spraints_advent_of_code_2019::intcode::{self, IntCodeMemory, Item};
 
 fn main() {
     println!("--------------");
@@ -7,13 +7,14 @@ fn main() {
 
     let program = intcode::read_program();
 
-    part1(&program);
+    run_part("PART 1", &program, vec![1]);
+    run_part("PART 2", &program, vec![2]);
 }
 
-fn part1(program: &IntCodeMemory) {
-    println!("PART 1");
+fn run_part(name: &str, program: &IntCodeMemory, inputs: Vec<Item>) {
+    println!("{}", name);
     println!("OUTPUT:");
-    for output in intcode::run_easy("part1", program.clone(), vec![1], false) {
+    for output in intcode::run_easy(name, program.clone(), inputs, false) {
         println!(" -> {}", output);
     }
 }
